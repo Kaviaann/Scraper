96172cdaa408811adda8ddf988f1687197da72d6
Ludwigyelstein <fayiedolshop@gmail.com>
2024-05-07 13:57:00 +0700

    Adding new data to animeSearch

--- example/index.mjs
+++ example/index.mjs
@@ -5,14 +5,16 @@ Scraper.searchAnime('Evangelion', (datas) => {
   if(!Buffer.compare(Buffer.from(datas), Buffer.from([]))) console.log(`Anime Not Found`)
 
   else {
-    // ? Show Result
-    datas.map((v, i) =>{
 
-      const animeInfo = `\n${i+1}. Title : ${v.title}\ntype : ${v.type},\nScore ( Rating ) : ${v.score},\nSeason : ${v.season}\nSynops : ${v.synops.split('.').slice(0, 2).join('.')}\nGenres : ${v.genres.join(', ')}`
-      console.log('\n' + '-'.repeat(20))
-      console.log(animeInfo)
+    // ? Show Result
+    const res = datas.map((v, i) =>{
+
+      return `\n${i+1}. Title : ${v.title}\nLink : ${v.link}\nThumbnail : ${v.thumb}\nType : ${v.type},\nScore ( Rating ) : ${v.score},\nSeason : ${v.season}\nSynops : ${v.synops.split('.').slice(0, 2).join('.')}\nGenres : ${v.genres.join(', ')}`
 
     })
+
+    console.log(res.join('\n\n' + '-'.repeat(20) + '\n'))
+
   }
 
 })
