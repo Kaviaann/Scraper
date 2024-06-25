d1fa56fcb508d4f5687de5dd56f336d6ce9409ba
Ludwigyelstein <fayiedolshop@gmail.com>
2024-06-25 10:24:06 +0700

    snackvid

---
+++ src/cjs/lib/installer.js
@@ -0,0 +1,39 @@
+const cheerio = require("cheerio");
+
+async function snackVideo(url) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!/snackvideo.com/gi.test(url)) return reject("Invalid URL!");
+      const res = await fetch(url).then((v) => v.text());
+      const $ = cheerio.load(res);
+      const video = $("div.video-box").find("a-video-player");
+      const author = $("div.author-info");
+      const attr = $("div.action");
+      const data = {
+        title: $(author)
+          .find("div.author-desc > span")
+          .children("span")
+          .eq(0)
+          .text()
+          .trim(),
+        thumbnail: $(video)
+          .parent()
+          .siblings("div.background-mask")
+          .children("img")
+          .attr("src"),
+        media: $(video).attr("src"),
+        author: $("div.author-name").text().trim(),
+        authorImage: $(attr).find("div.avatar > img").attr("src"),
+        like: $(attr).find("div.common").eq(0).text().trim(),
+        comment: $(attr).find("div.common").eq(1).text().trim(),
+        share: $(attr).find("div.common").eq(2).text().trim(),
+      };
+
+      resolve(data);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+snackVideo("https://s.snackvideo.com/p/j9jKr9dR");
--- src/cjs/lib/search.js
+++ src/cjs/lib/search.js
@@ -1,19 +1,45 @@
 const cheerio = require("cheerio");
 const BASE_URL = "https://genius.com";
 
-/**
- * SCRAPED BY KAVIAN
- * FORBIDDEN TO SELL OR DELETE MY WM
- */
-
-async function geniusSearch(query) {
+async function sticker(query, page = 1, type = "sticker") {
   return new Promise(async (resolve, reject) => {
+    if (
+      !["sticker", "icon", "animated-icon", "uicon"].includes(
+        type.trim().replace(/[ ]/gi, "-")
+      )
+    )
+      return reject("Invalid Type");
     try {
-      const res = await fetch(`${BASE_URL}/search?q=${encodeURI(query)}`).then(
-        (v) => v.text()
+      let res = await fetch(
+        `https://www.flaticon.com/search/${page}?word=${encodeURI(
+          query
+        )}&type=${type}`
       );
+      if (page !== 1 && !res.ok) return reject("Page Not Found");
+      if (!res.ok) return reject("Query Not Found");
+      res = await res.text();
       const $ = cheerio.load(res);
-      console.log(res);
+      const data = {
+        total_get: $("p#total_icon_badge").text().trim(),
+        page,
+        stickers: [],
+      };
+      $("ul.icons")
+        .find("li[data-id]")
+        .each((i, el) => {
+          const sticker = {};
+          const attr = $(el).attr();
+          for (let name of Object.keys(attr)) {
+            if (!name) continue;
+            const q = name.split("-");
+            if (q[0] !== "data") continue;
+            if (!attr[q.join("-")]) continue;
+            sticker[q[1]] = attr[q.join("-")];
+          }
+          data.stickers.push(sticker);
+        });
+
+      resolve(data);
     } catch (e) {
       reject(e);
     }
@@ -116,5 +142,6 @@ async function geniusLyric(title, artist) {
 }
 
 module.exports = {
+  sticker,
   geniusLyric,
 };
