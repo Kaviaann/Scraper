465be5c1dd4766d11a8e0c029079e059bba330e6
Ludwigyelstein <fayiedolshop@gmail.com>
2024-07-13 18:41:13 +0700

    AIOUpdate

--- src/esm/lib/downloader.d.ts
+++ src/esm/lib/downloader.d.ts
@@ -40,8 +40,41 @@ export declare interface ISnackVideo {
   share: String;
 }
 
+export declare interface ICobaltOptions {
+  audio: Boolean;
+  aFormat: "best" | "mp3" | "ogg" | "wav" | "opus";
+  vCodec: "standar" | "high" | "medium";
+  vReso:
+    | "max"
+    | "8k+?"
+    | "4k"
+    | "1440p?"
+    | "1080p?"
+    | "720p?"
+    | "480p?"
+    | "360p?"
+    | "240p?"
+    | "144p?";
+}
+
+export declare interface ICobaltDownload {
+  status: "error" | "redirect" | "stream" | "success" | "rate-limit" | "picker";
+  text?: String;
+  url?: String;
+  pickerType: "images" | "various";
+  picker: {
+    type?: "video" | "photo";
+    url: String;
+    thumb?: String;
+  };
+  audio: string;
+}
+
 export declare function terabox(url: String): Promise<ITerabox>;
 export declare function drive(url: String): Promise<IDrive>;
 export declare function mediafire(url: String): Promise<IMediafire>;
 export declare function snackVideo(url: String): Promise<ISnackVideo>;
-export declare function cobalt(url: String): Promise<Object>;
+export declare function cobalt(
+  url: String,
+  options: ICobaltOptions
+): Promise<ICobaltDownload>;
--- src/esm/lib/downloader.js
+++ src/esm/lib/downloader.js
@@ -105,9 +105,106 @@ async function snackVideo(url) {
   });
 }
 
-async function cobalt(url) {
+/**
+ * Scraped By Kaviaann
+ * Protected By MIT LICENSE
+ * Whoever caught removing wm will be sued
+ * @param {string} url
+ * @param {{
+ * audio : Boolean,
+ * aFormat : 'best'|'mp3'|'ogg'|'wav'|'opus',
+ * vCodec : 'standar'|'high'|'medium',
+ * vReso : 'max'|'8k\+?'|'4k'|'1440p?'|'1080p?'|'720p?'|'480p?'|'360p?'|'240p?'|'144p?'
+ * }} options
+ * @returns {
+ *  status : 'stream'|'success'|'picker'|'error'|'redirected'|'rate-limit'|
+ * }
+ * @author Kaviaann 2024
+ * @copyright https://whatsapp.com/channel/0029Vac0YNgAjPXNKPXCvE2e
+ */
+async function cobalt(
+  url,
+  options = {
+    audio: false,
+    aFormat: "mp3",
+    vCodec: "standar",
+    vReso: "720p",
+    mute: false,
+  }
+) {
   return new Promise(async (resolve, reject) => {
     try {
+      if (!url) return reject("Insert URL!");
+
+      // ? OPTIONS
+      let { audio, aFormat, vCodec, vReso, mute } = options;
+
+      const prop = {};
+      const data = {
+        url: url,
+        filenamePattern: "pretty",
+      };
+
+      // ? AUDIO
+      if (audio) {
+        const aFRegex = /best|mp3|ogg|wav|opus/gi;
+        if (!aFormat) aFormat = "mp3";
+        if (!aFRegex.test(aFormat)) aFormat = "mp3";
+        data.isAudioOnly = true;
+        data.aFormat = aFormat;
+        prop.type = "audio";
+        prop.mtype = aFormat;
+      }
+
+      // ? VIDEO
+      else {
+        // ? REGEXP
+        const vCRegex = /standar|high|medium/gi;
+        const vRRegex =
+          /max|8k\+?|4k|1440p?|1080p?|720p?|480p?|360p?|240p?|144p?/gi;
+
+        // ? IF
+        if (!vReso) vReso = "720p";
+        if (!vCodec) vCodec = "standar";
+        if (!vCRegex.test(vCRegex)) vCodec = "standar";
+        if (!vRRegex.test(vReso)) vReso = "720p";
+        if (!mute) mute = false;
+
+        // ? QUALITY
+        if (vReso === "8k+") vReso = "max";
+
+        // ? CODEC
+        switch (vCodec) {
+          case "standar": {
+            vCodec = "h246";
+            break;
+          }
+          case "high": {
+            vCodec = "av1";
+            break;
+          }
+          case "medium": {
+            vCodec = "vp9";
+            break;
+          }
+          default: {
+            vCodec: "h246";
+            break;
+          }
+        }
+
+        data.vCodec = vCodec;
+        data.vQuality = vReso;
+        data.isAudioOnly = false;
+        data.isAudioMuted = mute;
+        prop.type = "video";
+        prop.hd = /max|8k+?|4k|1440p?/gi.test(vReso);
+        prop.quality = vReso === "max" ? "8k+" : vReso;
+        prop.codec = vCodec;
+        prop.isMuted = mute;
+      }
+
+      // ? FETCHING
       const BASE_URL = "https://cobalt.tools";
       const BASE_API = "https://api.cobalt.tools/api";
       await fetch(BASE_API + "/json", {
@@ -120,7 +217,7 @@ async function cobalt(url) {
           origin: BASE_URL,
           referer: BASE_URL,
         },
-      }).then(async (v) => {
+      }).then(async () => {
         const res = await fetch(BASE_API + "/json", {
           method: "POST",
           headers: {
@@ -130,18 +227,19 @@ async function cobalt(url) {
             "content-type": "application/json",
             accept: "application/json",
           },
-          body: JSON.stringify({
-            url: url,
-            filenamePattern: "basic",
-          }),
+          body: JSON.stringify(data),
         }).then((v) => v.json());
 
-        return resolve(res)
+        return resolve({ ...res, ...prop });
       });
     } catch (e) {
       reject(e);
     }
   });
 }
+
+cobalt(
+  "https://www.tiktok.com/@velocitydrawing/photo/7266832970353233185?is_from_webapp=1&sender_device=pc&web_id=7367293095581648401"
+).then((v) => console.log(v));
 
 export { drive, mediafire, terabox, snackVideo, cobalt };
