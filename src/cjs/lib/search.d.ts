6747de4d8784fe4eedb00e11329ea0bb462d3987
Ludwigyelstein <fayiedolshop@gmail.com>
2024-07-18 21:15:28 +0700

    NPMScrape

--- src/cjs/lib/search.d.ts
+++ src/cjs/lib/search.d.ts
@@ -86,6 +86,211 @@ export declare interface INpmSearch {
   }[];
 }
 
+export declare interface INpmSearchs {
+  formData: {
+    search: {
+      q: {
+        value: String;
+      };
+    };
+  };
+  objects: {
+    package: {
+      author: {
+        name: String;
+        email: String;
+      };
+      name: String;
+      scope: String;
+      version: String;
+      description: String;
+      keywords: String[];
+      keywordsTruncated: Boolean;
+      date: {
+        ts: Number;
+        rel: String;
+      };
+      links: {
+        npm: String;
+        bugs: String;
+        homepage: String;
+        repository: String;
+      };
+      publisher: {
+        name: String;
+        avatars: {
+          small: String;
+          medium: String;
+          large: String;
+        };
+      };
+      maintainers: {
+        username: String;
+        email: StaticRange;
+      }[];
+      keywordsTruncated: true;
+    };
+    flags: {
+      insecure: 0;
+    };
+    score: {
+      final: Number;
+      detail: {
+        quality: Number;
+        popularity: Number;
+        maintenance: Number;
+      };
+    };
+    searchScore: Number;
+  }[];
+  total: Number;
+  time: String;
+  pagination: {
+    perPage: Number;
+    page: Number;
+  };
+  url: String;
+  hidePQM: null;
+  user: null;
+  auditLogEnabled: false;
+  userEmailVerified: null;
+  csrftoken: String;
+  notifications: [];
+}
+
+export declare interface INpmPackage {
+  canEditPackage: Boolean;
+  auditLogEnabled?: Boolean;
+  capsule: {
+    name: String;
+    description: String;
+    maintainers: String[];
+    "dist-tags": {
+      latest: String;
+      next?: String;
+    };
+    lastPublish: {
+      maintainer: String;
+      time: String;
+    };
+    types: { [key: String]: { package: String } };
+  };
+  dependents: {
+    dependentsCount: Number;
+    dependentsTruncated: String[];
+  };
+  downloads: {
+    downloads: Number;
+    label: String;
+  }[];
+  isStarred: Boolean;
+  ghapi: String;
+  isSecurityPlaceholder: Boolean;
+  linkingAllowedForPackage: Boolean;
+  package: String;
+  packageLinkingCallToActionHref: null;
+  packageUrl: String;
+  packageVersion: {
+    author: {
+      name: String;
+      avatars: {
+        large: String;
+        medium: String;
+        small: String;
+      };
+    };
+    description: String;
+    keywords: String[];
+    deprecations: String[];
+    dependencies: { [key: String]: String };
+    maintainers: {
+      name: String;
+      avatars: {
+        small: String;
+        medium: String;
+        large: String;
+      };
+    }[];
+    repository: String;
+    homepage: String;
+    name: String;
+    license: String;
+    version: String;
+    versions: String[];
+  };
+  packument: {
+    author: {
+      name: String;
+      avatars: {
+        large: String;
+        medium: String;
+        small: String;
+      };
+    };
+    description: String;
+    keywords: String[];
+    deprecations: String[];
+    dependencies: { [key: String]: String };
+    maintainers: {
+      name: String;
+      avatars: {
+        small: String;
+        medium: String;
+        large: String;
+      };
+    }[];
+    repository: String;
+    homepage: String;
+    name: String;
+    license: String;
+    version: String;
+    versions: {
+      version: String;
+      date: {
+        ts: Number;
+        rel: String;
+      };
+      dist: {
+        shasum: String;
+        fileCount: Number;
+        integrity: String;
+        tarball: String;
+        unpackedSize: Number;
+        signatures: {
+          keyid: String;
+          sig: String;
+        }[];
+      };
+    }[];
+    distTags: {
+      latest: String;
+      next: String;
+    };
+  };
+  private: Boolean;
+  isSecurityPlaceholder: Boolean;
+  provenance: {
+    enabled: Boolean;
+    feedbackUrl: String;
+  };
+  scope: String;
+  starAction: String;
+  versionsDownloads: { [key: String]: Number };
+  readme: {
+    data: String;
+    ref: "readme" | String;
+  };
+  undefined: Boolean;
+  documentContext: {
+    "readme.data": "readme";
+  };
+  user: null;
+  auditLogEnabled: Boolean;
+  userEmailVerified: null;
+  csrftoken: String;
+  notifications: [];
+}
+
 export declare function sticker(query: String): Promise<IStickerResponse>;
 export declare function geniusLyric(
   title: String,
@@ -103,3 +308,5 @@ export declare function webArchive(query: String): Promise<{
   };
 }>;
 export declare function npmSearch(query: String): Promise<INpmSearch[]>;
+export declare function npmSearch2(query: String): Promise<INpmSearchs>;
+export declare function npm(packageName: String): Promise<INpmPackage>;
